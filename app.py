fb7d306468e633403ce33583ad95e7065b2bb184
ericsjaffe <77411213+ericsjaffe@users.noreply.github.com>
2025-06-08 12:14:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, send_from_directory
+import json
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    with open("static_data.json") as f:
+        players = json.load(f)
+    return render_template('index.html', players=players)
 
-@app.route("/static_data.json")
-def static_data():
-    return send_from_directory(".", "static_data.json")
+@app.route('/static/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
