e138454287c340d7314773b01c23d2abcac59dff
ericsjaffe <77411213+ericsjaffe@users.noreply.github.com>
2025-06-08 11:10:52 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, jsonify
-from scraper import get_ranking_data
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def get_static_data():
+    with open("static_data.json", "r") as f:
+        return json.load(f)
+
 @app.route("/")
 def home():
-    updates = get_ranking_data()
+    updates = get_static_data()
     return render_template("index.html", updates=updates)
-
-@app.route("/api/updates")
-def api_updates():
-    return jsonify(get_ranking_data())
 
 if __name__ == "__main__":
     app.run(debug=True)
